eae1c5f386b15a5d3a11c25170f7bd0c104fda38
CorruptSausage <Slaats.Jordy@outlook.com>
2021-05-16 22:08:11 +0200

    Report command added.

--- src/main/java/sausagesmp/corruptsausage/sausagesmp/main/SausageSMP/reportCommand/ReportCommand.java
+++ src/main/java/sausagesmp/corruptsausage/sausagesmp/main/SausageSMP/reportCommand/ReportCommand.java
@@ -1,13 +1,47 @@
 package sausagesmp.corruptsausage.sausagesmp.main.SausageSMP.reportCommand;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ReportCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            if(p.hasPermission("core.report.send")){
+                if(args.length == 0){
+                    p.sendMessage(ChatColor.RED + "Format: /report <message>");
+                }else{
+                    for(Player players : Bukkit.getOnlinePlayers()){
+                        if(players.hasPermission("core.report.receive")){
+                            String message = "";
+                                for (int i = 1; i < args.length; i++){
+                                    message += args[i] + " ";
+                                }
+                                message = message.trim();
+                                players.sendMessage(ChatColor.YELLOW + "[Report] " + ChatColor.WHITE + ChatColor.AQUA + "(" + p.getDisplayName() + ")" + " " + ChatColor.GRAY + args[0] + " " + message);
+                                p.sendMessage(ChatColor.YELLOW + "Report has been send to the staff online!");
+                        }else{
+
+                        }
+                    }
+                }
+            }else{
+                p.sendMessage(ChatColor.RED + "You don't have access to this command.");
+            }
+
+        }else{
+            System.out.println("You are not a player.");
+        }
+
+
+
+
         return true;
     }
 }
